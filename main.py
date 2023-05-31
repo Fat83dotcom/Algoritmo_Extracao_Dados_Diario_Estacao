0ec2541c78c7a5df5a51a7347d2be1c9f3c8c99f
fat83dotcom <fernando.quimic@gmail.com>
2023-05-31 19:58:02 -0300

    Renomeação das instancias de classe

--- main.py
+++ main.py
@@ -5,23 +5,23 @@ from toolsClass import DataExtractor, ConverterMonths, DailyDate, DataModel
 if __name__ == '__main__':
     try:
         dB = OperationDataBase('dado_diario')
-        m = ConverterMonths()
-        r = FileRetriever('.')
-        d = DailyDate()
-        eX = DataExtractor()
-        eP = DataProcessor()
-        mD = DataModel(dB)
+        cM = ConverterMonths()
+        fR = FileRetriever('.')
+        dD = DailyDate()
+        dE = DataExtractor()
+        dP = DataProcessor()
+        dM = DataModel(dB)
 
-        day = d.extractDay(d.yesterdayDate())
-        year = d.extractYear(d.yesterdayDate())
-        month = m.getMonths(str(d.extractMonth(d.yesterdayDate())))
-        r.findYesterdayFile(month, year)
-        print(list(r.getFoundFiles()))
-        for file in r.getFoundFiles():
-            data: list = eX.extractedDailyData(file, int(day))
-            result = eX.getExtractData()
-            eP.processedData(result)
-            mD.executeDB(eP.getDataProcessed())
+        day = dD.extractDay(dD.yesterdayDate())
+        year = dD.extractYear(dD.yesterdayDate())
+        month = cM.getMonths(str(dD.extractMonth(dD.yesterdayDate())))
+        fR.findYesterdayFile(month, year)
+        print(list(fR.getFoundFiles()))
+        for file in fR.getFoundFiles():
+            data: list = dE.extractedDailyData(file, int(day))
+            result = dE.getExtractData()
+            dP.processedData(result)
+            dM.executeDB(dP.getDataProcessed())
 
         dB.closeConnection()
     except Exception as e:
