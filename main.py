9a0acf26f090d4b8a8ffac6984728b5416024950
fat83dotcom <fernando.quimic@gmail.com>
2023-06-25 21:38:33 -0300

    Refatoração do modulo main -Classe MainWorker

--- main.py
+++ main.py
@@ -1,51 +1,58 @@
 from toolsClass import DataProcessor, FileRetriever
 from toolsClass import DataExtractor, ConverterMonths, DailyDate
-from DataBaseManager.LogFiles import LogTimeMixin
+from DataBaseManager.LogFiles import LogTimeMixin, LogErrorsMixin
 from DataBaseManager.databaseSettings import dbCredentials
 from DataBaseManager.OperationalDataBase import DataModel
 from DataBaseManager.OperationalDataBase import OperationDataBase
 import os
 
 
-class MainWorker:
-    pass
+class MainWorker(LogTimeMixin, LogErrorsMixin):
+    def __init__(self) -> None:
+        try:
+            self.folderFiles = os.path.join(
+                'home', 'fernando', 'Estacao'
+            )
+            self.dB = OperationDataBase(dbCredentials(1))
+            self.dB2 = OperationDataBase(dbCredentials(2))
+            self.dB3 = OperationDataBase(dbCredentials(3))
+            self.cM = ConverterMonths()
+            self.fR = FileRetriever(f'/{self.folderFiles}')
+            self.dD = DailyDate()
+            self.dE = DataExtractor()
+            self.dP = DataProcessor()
+            self.dM = DataModel(self.dB)
+            self.dM2 = DataModel(self.dB2)
+            self.dM3 = DataModel(self.dB3)
+        except Exception as e:
+            className = self.__class__.__name__
+            methName = self.__init__.__name__
+            self.registerErrors(className, methName, e)
+
+    def run(self):
+        init = self.snapshotTime()
+        try:
+            self.registerTimeLogStart()
+            dDiario = 'dado_diario'
+            day = self.dD.extractDay(self.dD.yesterdayDate())
+            year = self.dD.extractYear(self.dD.yesterdayDate())
+            month = self.cM.getMonths(str(self.dD.extractMonth(self.dD.yesterdayDate())))
+            self.fR.findYesterdayFile(month, year)
+            for file in self.fR.getFoundFiles():
+                self.dE.extractedDailyData(file, int(day))
+                result = self.dE.getExtractData()
+                self.dP.processedData(result)
+                self.dM.execInsertDDiario(dDiario, self.dP.getDataProcessed())
+                self.dM2.execInsertDDiario(dDiario, self.dP.getDataProcessed())
+                self.dM3.execInsertDDiario(dDiario, self.dP.getDataProcessed())
+        except Exception as e:
+            print(e)
+        finally:
+            self.registerTimeLogEnd()
+            end = self.snapshotTime()
+            self.registerTimeElapsed(init, end)
 
 
 if __name__ == '__main__':
-    log = LogTimeMixin()
-    init = log.snapshotTime()
-    try:
-        log.registerTimeLogStart()
-        dBTable = 'dado_diario'
-        folderFiles = os.path.join(
-            'home', 'fernando', 'Estacao'
-        )
-        dB = OperationDataBase(dBTable, dbCredentials(1))
-        dB2 = OperationDataBase(dBTable, dbCredentials(2))
-        dB3 = OperationDataBase(dBTable, dbCredentials(3))
-        cM = ConverterMonths()
-        fR = FileRetriever(f'/{folderFiles}')
-        dD = DailyDate()
-        dE = DataExtractor()
-        dP = DataProcessor()
-        dM = DataModel(dB)
-        dM2 = DataModel(dB2)
-        dM3 = DataModel(dB3)
-
-        day = dD.extractDay(dD.yesterdayDate())
-        year = dD.extractYear(dD.yesterdayDate())
-        month = cM.getMonths(str(dD.extractMonth(dD.yesterdayDate())))
-        fR.findYesterdayFile(month, year)
-        for file in fR.getFoundFiles():
-            dE.extractedDailyData(file, int(day))
-            result = dE.getExtractData()
-            dP.processedData(result)
-            dM.executeInsertDadoDiarioTable(dP.getDataProcessed())
-            dM2.executeInsertDadoDiarioTable(dP.getDataProcessed())
-            dM3.executeInsertDadoDiarioTable(dP.getDataProcessed())
-    except Exception as e:
-        print(e)
-    finally:
-        log.registerTimeLogEnd()
-        end = log.snapshotTime()
-        log.registerTimeElapsed(init, end)
+    _exec = MainWorker()
+    _exec.run()
